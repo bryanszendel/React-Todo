e1df54c1ae5f9209297c9511aa78b27cb5c751e0
ebisLab <ebaiden@techie.com>
2019-08-21 13:57:37 -0400

    emoji added

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -8,7 +8,7 @@ const TodoList = props => {
         <Todo key={item.id} item={item} toggleItem={props.toggleItem}/>
       ))}
       <button className="clear-btn" onClick={props.clearCompleted}>
-        Clear Completed
+        Clear Completed  😃
       </button>
     </div>
   )
